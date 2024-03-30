c86fd82cb8ff6a2b3452a8d92e180f032e7650c8
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-03-29 21:40:07 -0700

    savethedate

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Hero from './Hero.tsx'
+import SaveTheDate from './SaveTheDate.tsx'
 import Header from './Header.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Hero />
+    <SaveTheDate />
   </React.StrictMode>,
 )
