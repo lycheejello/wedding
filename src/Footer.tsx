65540aee0c13b2787fb049295b428c8cf8b349d9
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-03-29 21:47:50 -0700

    footer

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+
+function Footer() {
+    return (
+      <div className="footer">
+            <img className="foot-logo" src="https://storage.googleapis.com/wedding-web/Logo550.png" alt="Huyson-Arkina" />
+      </div>
+    )
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Hero from './Hero.tsx'
 import SaveTheDate from './SaveTheDate.tsx'
-import Header from './Header.tsx'
+import Location from './Location.tsx'
+import Footer from './Footer.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Hero />
     <SaveTheDate />
+    <Location />
+    <Footer />
   </React.StrictMode>,
 )
