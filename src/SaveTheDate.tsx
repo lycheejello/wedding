948695ca99ed442dbd84a93599becd59f1ac0b30
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-08-03 21:15:27 -0700

    center save the date

--- src/SaveTheDate.tsx
+++ src/SaveTheDate.tsx
@@ -4,24 +4,26 @@ import './SaveTheDate.css'
 function SaveTheDate() {
     return (
       <div className="saveTheDateContainer">
-        <div className="left">
-            <img className="leftImg" src="https://storage.googleapis.com/wedding-web/Photo2.png" alt="Huyson-Arkina" />
-        </div>
-        <div className="topRight">
-            <div>
-                <div>
-                    <p className="dateOnAt dateText">on</p>
-                    <p className="dateText">JAN.</p>
-                </div>
-                <p className="dateText">11, 2025</p>
-                <div>
-                    <p className="dateOnAt dateText">at</p>
-                    <p className="dateText">6 pm</p>
-                </div>
-            </div>
-        </div>
-        <div className="btmRight">
-            <img className="rightImg" src="https://storage.googleapis.com/wedding-web/Photo3.png" alt="Huyson-Arkina" />
+        <div className="saveTheDate">
+          <div className="left">
+              <img className="leftImg" src="https://storage.googleapis.com/wedding-web/Photo2.png" alt="Huyson-Arkina" />
+          </div>
+          <div className="topRight">
+              <div>
+                  <div>
+                      <p className="dateOnAt dateText">on</p>
+                      <p className="dateText">JAN.</p>
+                  </div>
+                  <p className="dateText">11, 2025</p>
+                  <div>
+                      <p className="dateOnAt dateText">at</p>
+                      <p className="dateText">6 pm</p>
+                  </div>
+              </div>
+          </div>
+          <div className="btmRight">
+              <img className="rightImg" src="https://storage.googleapis.com/wedding-web/Photo3.png" alt="Huyson-Arkina" />
+          </div>
         </div>
       </div>
     )
