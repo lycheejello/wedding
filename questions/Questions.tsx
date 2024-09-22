a6d449277ee3a7aae98a714c93eada962f3f4efd
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-09-22 15:13:28 -0700

    Q+A

--- questions/Questions.tsx
+++ questions/Questions.tsx
@@ -2,10 +2,35 @@ import './Questions.css'
 
 function Questions() {
     return (
-      <div className="questionsContainer">
-          <h2> Why is this page empty? </h2>
-          <h3> I'm working on it...</h3>
-      </div>
+    <main>
+        <h1>So you've got questions..</h1>
+
+        <section text-labelledby="dress-code-weather">
+            <h2 id="dress-code-weather">What's the dress code? What's the weather?</h2>
+            <p>Conservative Semi-Formal. We'll be in Ao Dai. Let us know if you want help shopping. Think Ao Dai, Maxi Dresses, Jumpsuits, button up shirt, blazer optional. It's gonna be hot and humid.</p>
+        </section>
+
+        <section text-labelledby="visa">
+            <h2 id="visa">Do I need a visa for visiting Vietnam?</h2>
+            <p>Yes. Let us know if you'd like help with this.</p>
+        </section>
+
+        <section text-labelledby="transportation">
+            <h2 id="transportation">Is there transportation to and from the hotels to the ceremony?</h2>
+            <p>We'll arrange transport from downtown to the tea ceremony. We suggest that you stay closeby to the reception.</p>
+        </section>
+
+        <section text-labelledby="hotel-booking">
+            <h2 id="hotel-booking">What hotel should I book?</h2>
+            <p>We are staying at the JW Marriott, with the group code XXXXX. If you choose to stay at a different hotel, there are a number of decent hotels by the wedding venue. </p>
+        </section>
+
+        <section text-labelledby="apps-download">
+            <h2 id="apps-download">What apps should I download for getting around in Ho Chi Minh?</h2>
+            <p><strong>Zalo</strong> - Messaging app of choice in Vietnam. We'll be communicating over this, so please download it!</p>
+            <p><strong>Grab</strong> - rideshare and food delivery. Accepts Cash. You can choose a Grab taxi for up to 3 or a Grab bike for 1.</p>
+        </section>
+    </main>
     )
 }
 
