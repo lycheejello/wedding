0036b74c5312c28023f804bc313b87d4d80a255c
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2024-09-22 16:40:30 -0700

    maps and rsvp footer

--- maps/Maps.tsx
+++ maps/Maps.tsx
@@ -2,10 +2,85 @@ import './Maps.css'
 
 function Maps() {
     return (
-      <div className="mapsContainer">
-          <h2> GEM center, more details soon! </h2>
-          <iframe width="100%" height="800" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=800&amp;hl=en&amp;q=8%20%C4%90.%20Nguy%E1%BB%85n%20B%E1%BB%89nh%20Khi%C3%AAm,%20%C4%90a%20Kao,%20Qu%E1%BA%ADn%201,%20H%E1%BB%93%20Ch%C3%AD%20Minh,%20Vietnam+(GEM%20center)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-      </div>
+    <body>
+    <div class="MapsContainer">
+        <h2>Itinerary</h2>
+
+        <table role="table" aria-label="Wedding Day Schedule">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Item</th>
+                    <th scope="col">Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Jan 10, Friday</td>
+                    <td>06:00 PM</td>
+                    <td>Welcome Dinner</td>
+                    <td>Welcome dinner at the heart of Ho Chi Minh City.<br />
+                        <span class="notes">Dress Code: Conservative Semi-Formal, Ao dai</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Jan 11, Saturday</td>
+                    <td>07:00 AM</td>
+                    <td>Tea Ceremony</td>
+                    <td>Tea ceremony at the bride's house in the outskirts of the city.<br />
+                        Following the ceremony is a light brunch of traditional Vietnamese food.<br />
+                        <span class="notes">Dress Code: Semi Formal, Ao Dai</span><br />
+                        Transportation to/from the venue will be provided from select hotels in downtown!
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>06:00 PM</td>
+                    <td>Reception Dinner</td>
+                    <td>Gem Center. Bottomless Beer, 2.5 hours <br />
+                        <span class="notes">Dress Code: Semi Formal, Ao Dai</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>10:00 PM</td>
+                    <td>After Party / Nhậu</td>
+                    <td>Beer garden that overlooks the city. Grill, seafood. Drinks.<br />
+                        Transportation to/from the venue will be provided from Gem Center.
+                    </td>
+                </tr>
+                <tr>
+                    <td>Jan 12, Sunday</td>
+                    <td></td>
+                    <td>Food Tour</td>
+                    <td>We are inviting our guests to join us for a food tour focusing on Vietnamese cuisine.</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div>
+            <h2>Welcome Dinner</h2> 
+            <h3>Mạ Quán</h3> 
+            <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=L%E1%BA%A7u%201,%20153/02%20%C4%90%C6%B0%E1%BB%9Dng%20%C4%90%E1%BB%93ng%20Kh%E1%BB%9Fi,%20Ph%C6%B0%E1%BB%9Dng%20B%E1%BA%BFn%20Ngh%C3%A9,%20Qu%E1%BA%ADn%201,%20Tp.%20H%E1%BB%93%20Ch%C3%AD%20Minh,%20Th%C3%A0nh%20ph%E1%BB%91%20H%E1%BB%93%20Ch%C3%AD%20Minh,%20H%E1%BB%93%20Ch%C3%AD%20Minh%2070000,%20Vietnam+(Bride's%20house)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        </div>
+
+        <div>
+            <h2>Tea Ceremony</h2> 
+            <h3>Brides's house</h3> 
+            <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=328%20HL2,%20phuoc%20vinh%20an,%20cu%20chi+(Bride's%20house)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        </div>
+
+        <div>
+            <h2>Reception</h2> 
+            <h3>GEM center</h3> 
+            <iframe width="100%" height="800" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=800&amp;hl=en&amp;q=8%20%C4%90.%20Nguy%E1%BB%85n%20B%E1%BB%89nh%20Khi%C3%AAm,%20%C4%90a%20Kao,%20Qu%E1%BA%ADn%201,%20H%E1%BB%93%20Ch%C3%AD%20Minh,%20Vietnam+(GEM%20center)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        </div>
+
+    </div>
+
+    </body>
+
     )
 }
 
--- questions/Questions.tsx
+++ questions/Questions.tsx
@@ -3,21 +3,32 @@ import './Questions.css'
 function Questions() {
     return (
     <main>
-        <h1>So you've got questions..</h1>
+        <h1>So you've got questions</h1>
 
         <section text-labelledby="dress-code-weather">
-            <h2 id="dress-code-weather">What's the dress code? What's the weather?</h2>
-            <p>Conservative Semi-Formal. We'll be in Ao Dai. Let us know if you want help shopping. Think Ao Dai, Maxi Dresses, Jumpsuits, button up shirt, blazer optional. It's gonna be hot and humid.</p>
+            <h2 id="dress-code-weather">What's the dress code for the Reception and Tea Ceremony?</h2>
+            <p>Semi-Formal. We'll be in Ao Dai. Let us know if you want help shopping. Think Ao Dai, Maxi Dresses, Jumpsuits, button up shirt, blazer optional. It's gonna be hot and humid.</p>
         </section>
 
+        <section>
+            <h2>Tea Ceremony? Am I Invited?</h2>
+            <p>Yes! Let me know if you've got other plans. Semi Formal. Food provided.</p>
+        </section>
+
+        <section >
+            <h2>Hows the weather?</h2>
+            <p>It's gonna be hot and humid.</p>
+        </section>
+
+
         <section text-labelledby="visa">
-            <h2 id="visa">Do I need a visa for visiting Vietnam?</h2>
-            <p>Yes. Let us know if you'd like help with this.</p>
+            <h2 id="visa">Do I need a visa for Vietnam?</h2>
+            <p>Yup. Let us know if you'd like help with this.</p>
         </section>
 
         <section text-labelledby="transportation">
-            <h2 id="transportation">Is there transportation to and from the hotels to the ceremony?</h2>
-            <p>We'll arrange transport from downtown to the tea ceremony. We suggest that you stay closeby to the reception.</p>
+            <h2 id="transportation">How do I get around?</h2>
+            <p>We'll arrange transport from downtown to the tea ceremony. We suggest that you stay closeby to the reception. For everythign else, use Grab.</p>
         </section>
 
         <section text-labelledby="hotel-booking">
@@ -26,9 +37,14 @@ function Questions() {
         </section>
 
         <section text-labelledby="apps-download">
-            <h2 id="apps-download">What apps should I download for getting around in Ho Chi Minh?</h2>
+            <h2 id="apps-download">What apps should I download?</h2>
             <p><strong>Zalo</strong> - Messaging app of choice in Vietnam. We'll be communicating over this, so please download it!</p>
             <p><strong>Grab</strong> - rideshare and food delivery. Accepts Cash. You can choose a Grab taxi for up to 3 or a Grab bike for 1.</p>
+        </section>
+
+        <section>
+            <h2>What's all this about a food tour?</h2>
+            <p>We'll have a car and roam around to different spots to eat and see the city.</p>
         </section>
     </main>
     )
