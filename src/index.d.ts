099403330f6314e9a11262b518d9e18cf14e1aa1
Huyson Lam <5705943+lycheejello@users.noreply.github.com>
2025-05-08 10:02:29 -0700

    missing brace

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,3 @@
 declare module '*.jpg'
 declare module '*.png'
-/**
- * An SVG document.
- *
- * The default export from the module will be the React component.
- *
- * You can also import the `ReactComponent` from the module, which will be the
- * React component itself.
- *
- * @packageDocumentation
- */
 declare module '*.svg'
